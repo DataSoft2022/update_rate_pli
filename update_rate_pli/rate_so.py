b2c33d261909f20e51ca984cfe837edbada523c0
msploit0x00 <minasploit1@outlook.com>
2024-07-22 16:43:31 +0300

    updating grand total and taxes

--- update_rate_pli/rate_so.py
+++ update_rate_pli/rate_so.py
@@ -4,44 +4,7 @@ from frappe.model.document import Document
 from frappe import _
 from erpnext.setup.utils import get_exchange_rate
 from frappe.utils import add_days, cint, cstr, flt, get_link_to_form, getdate, nowdate, strip_html
-
-# @frappe.whitelist(allow_guest=True)
-# def update_conversion_rate(sales_order_name, new_conversion_rate, handler=None, ignore_permission=True):
-#     try:
-#         # Get the Sales Order document
-#         user_roles = frappe.get_roles(frappe.session.user)
-        
-#         # Ensure the user has permission to update Sales Orders
-#         if 'submit' in user_roles:  # Assuming 'submit' is a role name that grants permission
-#             so = frappe.get_doc('Sales Order', sales_order_name)
-#             so.flags.ignore_validate_update_after_submit = True
-#             so.flags.ignore_validate = True
-#             so.flags.ignore_mandatory = True
-#             # Update the conversion rate
-#             so.conversion_rate = new_conversion_rate
-#             print(so.conversion_rate)
-#             # Update all items in the Sales Order
-#             for item in so.items:
-#                 item.base_rate = new_conversion_rate
-              
-            # print("nnnnnnnnnnnnnnnnnnnn")
-            # company_currency = frappe.db.get_value(
-            #         "Company", filters={"name": so.company}, fieldname=["default_currency"]
-            #     )
-            # print(company_currency)    
-            # print(get_exchange_rate(so.currency, company_currency, args="for_buying"))
-            # print("nnnnnnnnnnnnnnnnnnnn")  
-#             # Save the Sales Order document
-#             so.save(ignore_permissions=True)
-#             frappe.db.commit()
-            
-#             frappe.msgprint(_("Conversion rate updated successfully"))
-#         else:
-#             frappe.throw(_("You are not authorized to update Sales Orders"))
-    
-#     except Exception as e:
-#         frappe.log_error(frappe.get_traceback(), _("Error in update_conversion_rate"))
-#         return {'error': str(e)}
+from frappe.utils import money_in_words
 
 
 @frappe.whitelist(allow_guest=True)
@@ -55,9 +18,20 @@ def update_conversion_rate(sales_order_name, new_conversion_rate):
     so.db_set("conversion_rate",new_conversion_rate)
     so.db_set("base_total",flt(new_conversion_rate) * flt(so.total))
     # so.db_set("base_total",new_conversion_rate)
-
+    so.db_set("base_total_taxes_and_charges",flt(new_conversion_rate) * flt(so.total_taxes_and_charges))
+    so.db_set("base_grand_total",flt(new_conversion_rate) * flt(so.grand_total))
+    so.db_set("base_rounded_total",flt(new_conversion_rate) * flt(so.rounded_total))
+    print(so.company_currency)
+    print(so.base_rounded_total)
+    so.db_set("base_in_words",money_in_words(so.base_rounded_total,so.company_currency))
+    
     # set Conversion Rate Manually From Popup Into child items
     for item in so.items:
         item.db_set("base_rate",(flt(item.rate) * flt(new_conversion_rate)))
         item.db_set("base_amount",(flt(item.amount) * flt(new_conversion_rate)))
+    for tax in so.taxes:
+        tax.db_set("base_tax_amount",flt(tax.tax_amount)*flt(new_conversion_rate))    
+        tax.db_set("base_total",flt(tax.total)*flt(new_conversion_rate))
+
+
     frappe.db.commit()
